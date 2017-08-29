a23ffff33a9e78f62da96120eed9858d4bee312c
이형훈 <skt1111355@ihyeonghun-ui-MacBook-Pro.local>
2017-08-29 17:06:22 +0900

    add log

--- src/main/java/bootwildfly/Application.java
+++ src/main/java/bootwildfly/Application.java
@@ -8,19 +8,11 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
     }
 }
 
--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -2,13 +2,17 @@ package bootwildfly;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class HelloWildFlyController {
+	private static final Logger LOGGER = LoggerFactory.getLogger(HelloWildFlyController.class);
 
 
     @RequestMapping("hello")
     public String sayHello(){
+		Logger.info("sayHello is called");
         return ("Hello, Magnus SpringBoot app on Wildfly, v1");
     }
 }
